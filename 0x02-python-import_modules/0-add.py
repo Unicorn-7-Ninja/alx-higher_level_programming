b1e7835643cf40cd542bd58567f3661cf5a2710d
Unicorn-7-Ninja <122676935+Unicorn-7-Ninja@users.noreply.github.com>
2024-01-11 00:54:02 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+   def add(a, b):
    """Print the sum of 1 and 2."""
    from add_0 import add
-def add(a, b):
    
 a = 1
 b = 2
+
 result = add(a, b)
 
 print("{} + {} = {}".format(a, b, result))
